1804a6013920f91fa4895372f4fc39cadd776653
ixixii <ixixii@sina.cn>
2025-05-13 04:57:13 +0800

    专属错题本_大于3次

---
+++ auto_错题本_大于3次.js
@@ -0,0 +1,443 @@
+专属定制错词本
+
+1、scowl 错误次数：6
+/skaʊl/ n. 愁容，怒容，暴风雨前的阴沉晦暗 v. 皱眉头，怒目而视
+The scowl on my face means I'm not happy with your performance.
+我脸上的怒容表示我对你的表现不满意。
+
+2、ethos 错误次数：5
+/ˈiːθɑːs/ n. 民族精神，气质，社会思潮；道德准则
+The two volunteers want to spread the ethos of recycling.
+两个志愿者希望传播回收利用的理念。
+
+3、vicarious 错误次数：5
+/vaɪˈkeriəs/ adj. 代替别人做的；代理的，代替的；间接感受到的
+Reading travel books can provide a vicarious experience of different cultures.
+阅读旅行书籍可以提供不同文化的间接体验。
+
+4、hustle 错误次数：4
+/ˈhʌsl/ vt. 催促；硬挤，乱推；硬逼，逼使；强夺 vi. 赶紧；硬挤过去；拼命挣钱；拼命
+干，努力干 n. 忙碌，奔忙；挤，推；拥挤喧嚷
+He has to hustle so he won't be late for the meeting.
+他必须加快速度以免会议迟到。
+
+5、speckle 错误次数：4
+/ˈspekl/ n. (皮肤、鸟的羽毛或鸡蛋上的)色斑 v. 使弄上斑点，沾污
+I always forget — is a banana better if it's speckled, or worse?
+我总是记不住-香蕉有斑点是好还是不好？
+
+6、ambivalent 错误次数：4
+/æmˈbɪvələnt/ adj. 矛盾的；摇摆不定的；好恶相克的 n. 两性人
+I'm feeling ambivalent about whether I want wine or milk.
+在选择是要牛奶还是红酒的时候，我感到很矛盾。
+
+7、manifestation 错误次数：4
+/ˌmænɪfeˈsteɪʃn/ n. 表现，表示；显灵；示威运动
+Language is both a manifestation and a carrier of national culture.
+语言既是民族文化的表现，又是民族文化的载体。
+
+8、quaint 错误次数：4
+/kweɪnt/ adj. 精巧的；古雅的What a quaint town with its narrow streets and old-fashioned houses！
+这真是一个古雅的小镇啊！有着狭窄的街道和老式的房屋。
+
+9、gallant 错误次数：4
+/ˈgælənt/, /gəˈlænt/ adj. 勇敢的；（男子对女子）殷勤的（读作/ˈgælənt/） n. （尤指对女子殷勤的）
+时髦男子
+The gallant knight is rescuing the trapped princess.
+勇敢的骑士正在营救被困的公主。
+
+10、hype 错误次数：4
+/haɪp/ n. 大肆宣传 v. 大肆宣传；使…兴奋
+He uses all media resources to promote the hype.
+他利用所有媒体资源来大肆宣传。
+
+11、ineluctable 错误次数：4
+/ˌɪnɪˈlʌktəbl/ adj. 不可改变的，无法逃避的
+Death is an ineluctable fact of life.
+死亡是人生无法逃避的事实。
+
+12、feckless 错误次数：4
+/ˈfekləs/ adj. 不负责任的，无目标无计划的，无效的 adv. 没有价值地；没有长远目标地；
+不负责任地 n. 没有价值，没有长远目标，不负责任
+The man was regarded as clumsy and feckless.
+这个男人笨手笨脚、一无是处。
+
+13、misgiving 错误次数：4
+/ˌmɪsˈɡɪvɪŋ/ n. 疑虑 vt. 担忧；使…疑虑；害怕（misgive的变形）
+Many employees expressed misgivings about the method of assessment.
+许多员工对考核方式很是担忧。
+
+14、potent 错误次数：4
+/ˈpoʊtnt/ adj. 强有力的；有权势的；有说服力的；强效力的，烈性的
+He's a potent speaker.
+他是一位有说服力的演讲者。
+
+15、forestall 错误次数：4
+/fɔːrˈstɔːl/ v. 领先；占先一步；先发制人，阻止，妨碍；垄断；占先一步
+We launched a space mission to forestall the impact of the comet.
+我们发射了一次太空任务来预先阻止彗星撞向地球。
+
+16、sententious 错误次数：4/senˈtenʃəs/ adj. 好说教的，简要的，警句的
+He is so sententious; he is always trying to show off how smart he is.
+他非常爱说教，总是想炫耀他有多聪明。
+
+17、prattle 错误次数：4
+/ˈprætl/ v. 闲聊；胡说；小孩般说话 n. 无聊话；咿咿呀呀声
+The women love to drink coffee and prattle to each other.
+女人们都喜欢一起喝喝咖啡，聊聊天。
+
+18、somnolent 错误次数：4
+/ˈsɑːmnələnt/ adj. 思睡的，催眠的 adv. 瞌睡地；昏昏欲睡地；使人瞌睡地；催眠地
+I'm always a little somnolent until I've had two strong cups of coffee.
+在喝完两杯浓咖啡之前，我总是有点困倦。
+
+19、disagreeable 错误次数：3
+/ˌdɪsəˈɡriːəbl/ adj. 讨厌的；难相处的 n. 令人不快的事情（或情况、经历或特性等）；讨厌的人
+He thinks that the smell of this canned ﬁsh is highly disagreeable.
+他觉得这种鲱鱼罐头超级难闻！
+
+20、irreverent 错误次数：3
+/ɪˈrevərənt/ adj. 不敬的，无礼的
+Standing on the bronze winged lion seems a little irreverent.
+站在青铜飞狮像上，感觉有点不敬啊。
+
+21、pedagogic 错误次数：3
+/ˌpedəˈɡɑːdʒɪk/ adj. 教育学的
+Our teacher uses many pedagogic techniques to make his lessons more interesting.
+我们的老师运用很多教育学方法来使课堂更加有趣。
+
+22、exalt 错误次数：3
+/ɪɡˈzɔːlt/ v. 提升，提拔，赞扬，使得意
+Through his efforts, he was exalted step by step in the workplace.
+通过他的努力，他在职场上一步步晋升。
+
+23、ﬁnery 错误次数：3
+/ˈfaɪnəri/ n. 服饰，华丽的服饰，鲜艳服装，装饰
+Don't I look quite the lady, dressed in all my expensive ﬁnery?
+穿上了所有最贵的服饰，我看起来是不是很淑女？
+
+24、imbecile 错误次数：3/ˈɪmbəsl/ n. 低能者，愚蠢的人 adj. 低能的，愚笨的，虚弱的
+What an imbecile! Why would you take a nap on the train tracks?
+多么愚蠢的人！为什么你要在火车铁轨上睡觉？
+
+25、rationale 错误次数：3
+/ˌræʃəˈnæl/ n. 基本原理；原理的阐述
+This chemical equation explains the rationale behind the phenomena.
+这个化学方程式解释了这个现象的根本原因。
+
+26、morbid 错误次数：3
+/ˈmɔːrbɪd/ adj. 疾病的；（精神等）病态的；恐怖的
+She often fell into a morbid state.
+她时常陷入一种病态的状态。
+
+27、amoral 错误次数：3
+/ˌeɪˈmɔːrəl/ adj. 不知是非的
+It takes an amoral person to steal a car: he doesn't care whether it's right or wrong.
+一个不知是非的人才会偷车：他根本不在乎这事正确与否。
+
+28、repression 错误次数：3
+/rɪˈpreʃn/ n. 压抑，压制，镇压
+Slaves had a very low social status and were under constant repression.
+奴隶的社会地位很低并且长期受到压迫。
+
+29、critique 错误次数：3
+/krɪˈtiːk/ n. 批评；评论文章；评论，批评 v. 批判，评论；评论
+Don't be upset when someone critiques your work.
+当有人批评你的工作时，别沮丧。
+
+30、daffodil 错误次数：3
+/ˈdæfədɪl/ n. 水仙花 adj. 水仙花色的
+Yellow daffodils are my favorite ﬂower!
+黄水仙是我最爱的花！
+
+31、emulate 错误次数：3
+/ˈemjuleɪt/ v. 努力赶上（超过）；仿效；【计】仿真 n. 仿真
+Teachers are role models for children to emulate.
+教师是孩子们效仿的榜样。
+
+32、dispensable 错误次数：3/dɪˈspensəbl/ adj. 不必要的，可有可无的
+We put all our dispensable waste in the trash can.
+我们把所有不必要的弃物扔进垃圾桶里。
+
+33、deviant 错误次数：3
+/ˈdiːviənt/ adj. 变态的，反常的（多指性行为）；越出常规的 n. 异常的人；性变态者
+The cat was considered deviant by its own kind for acting like a dog.
+这只猫因为表现得像狗而被同类视为变态。
+
+34、sensuous 错误次数：3
+/ˈsenʃuəs/ adj. 感觉上的，依感观的，诉诸美感的
+4D movies give the audience a far more sensuous experience.
+4D电影给观众带来更棒的感觉上的体验。
+
+35、rectitude 错误次数：3
+/ˈrektɪtuːd/ n. 正直，公正，正确
+A judge must be a person of total rectitude.
+法官必须是一个完全公正之人。
+
+36、boggle 错误次数：3
+/ˈbɑːɡl/ v. 畏缩不前；惊恐；使……困惑
+Seeing this weird creature has just boggled my mind.
+看见这种奇怪的生物令我十分地困惑。
+
+37、pretentious 错误次数：3
+/prɪˈtenʃəs/ adj. 做作的，自命不凡的，炫耀的
+Get that pretentious look off your face! You're not cool!
+别摆着一副自命不凡的样子！你一点都不酷！
+
+38、dire 错误次数：3
+/ˈdaɪər/ adj. 可怕的；悲惨的；极端的；极其严重的
+These meteors will have dire consequences for life on Earth.
+这些流星将对地球上的生命造成严重的影响。
+
+39、incoming 错误次数：3
+/ˈɪnkʌmɪŋ/ adj. 引入的，到来的，打入的 n. 进来，到来，收入
+Message from mother incoming! I hope she's not angry at me!
+妈妈的短信来了！希望她别生我的气！
+
+40、haggard 错误次数：3/ˈhæɡərd/ adj. 憔悴的，消瘦的 n. 哈格德；未驯服的鹰；悍鹰
+Sleeping out in the open gives you a haggard look.
+户外睡觉使你一副憔悴的样子。
+
+41、exemplary 错误次数：3
+/ɪɡˈzempləri/ adj. 模范的，可仿效的
+The juniors look up to him because he is an exemplary senior.
+学弟学妹们很尊敬他，因为他是一位模范学长。
+
+42、ignominy 错误次数：3
+/ˈɪɡnəmɪni/ n. 耻辱，不体面，丑行
+What an ignominy for Brazil to be defeated 7-1 in the football match.
+对巴西队而言，在足球比赛中以1比7的比分被击败真是奇耻大辱。
+
+43、conﬂate 错误次数：3
+/kənˈﬂeɪt/ v. 合并
+The doctor conﬂates traditional Chinese medicine and modern western medicine.
+这个医生结合了传统中药和现代西药。
+
+44、foreboding 错误次数：3
+/fɔːrˈboʊdɪŋ/ n. 预感，预兆 adj. 预感的，不祥之兆的
+The sky was dark and foreboding; a storm would come soon.
+天空很暗、充满了预兆：暴风雨即将来临。
+
+45、mortuary 错误次数：3
+/ˈmɔːrtʃueri/ n. 太平间，停尸间 adj. 死的，悲哀的
+There are many corpses lying in the mortuary.
+停尸间里面躺着很多尸体。
+
+46、ascertain 错误次数：3
+/ˌæsərˈteɪn/ v. 探知，弄清，查明（真相等）；探知，弄清，查明(真相等)；弄清，查明(真相
+等)
+The detective searched this room to ascertain the facts of the case.
+侦探搜查了这个房间以便查明案情。
+
+47、interlock 错误次数：3
+/ˌɪntərˈlɑːk/ v. 连锁，连串；交错，联结 n. 〔电信〕结合；联锁装置；相关；联动
+Mary sits with her ﬁngers interlocked under her chin.
+
+玛丽坐在那里，十指相扣撑着下巴。48、injunction 错误次数：3
+/ɪnˈdʒʌŋkʃn/ n. 【法】指令，禁令
+This latest injunction states that this area bans dog walking.
+最新的条令规定这片区域禁止遛狗。
+
+49、viable 错误次数：3
+/ˈvaɪəbl/ adj. 能生育的；能养活的；可行的；可实施的；可生存的；可以实施的；渴望成
+功的
+She deemed the subway a viable alternative option for when the trafﬁc was bad.
+她认为在交通拥挤的时候，乘地铁是一个可行的选择。
+
+50、assertive 错误次数：3
+/əˈsɜːrtɪv/ adj. 肯定的；决断的；坚定而自信的；独断的；坚定自信的
+The head chef is very assertive and gives everyone clear instructions on what to do.
+主厨是个非常决断的人，清楚地告诉每个人该做什么。
+
+51、demeanor 错误次数：3
+/dɪ'miːnə/ n. 举止，行为，风度
+We can tell by their demeanor how they did in the exam.
+我们可以通过他们的行为举止来判断他们考得怎么样。
+
+52、evasive 错误次数：3
+/ɪˈveɪsɪv/ adj. 逃避的，避而不谈的；托辞的，推托的
+The famous singer was evasive when he was asked the sensitive question.
+这位知名歌手对这个敏感的问题采取回避的态度。
+
+53、beastly 错误次数：3
+/ˈbiːstli/ adj. 讨厌的，令人不快的 adv. 非常
+Why does he insist on destroying other people's things? How beastly!
+他为什么总是要破坏别人的东西呢？真可恶！
+
+54、glazed 错误次数：3
+/ɡleɪzd/ adj. 光滑的；像玻璃的；上过釉的；呆滞无神的
+The glazed surface of pastry made my mouth water.
+点心那光滑的表面让我直流口水。
+
+55、semiliterate 错误次数：3
+/ˌsemɪ'lɪtərɪt/ adj. 半文盲的；有初等文化的；知识浅薄的
+This semiliterate man can only read a few words.
+这个半文盲的男人识字不多。
+
+56、civility 错误次数：3
+/səˈvɪləti/ n. 谦恭；礼貌；端庄
+Tipping one's hat to a lady is an old-fashioned gesture of civility.
+向女士倾斜帽子是老式的礼仪手势。
+
+57、slavish 错误次数：3
+/ˈsleɪvɪʃ/ adj. 照搬的，刻板模仿的；盲从的
+The employees who are slavish followers of the rules are like robots .
+员工们就像机器人一样，盲目遵守规则。
+
+58、contest 错误次数：3
+/ˈkɑːntest/, /kənˈtest/ n. 比赛，竞争（读作/ˈkɑːntest/）；竞赛 v. 争夺，争论（读作/kənˈtest/）；争取
+赢得（比赛、选举等）；争辩
+The girl in the middle seems to be the winner of the contest.
+中间的女生好像是这场比赛的获胜者。
+
+59、diverse 错误次数：3
+/daɪˈvɜːrs/ adj. 多样的；丰富的；多种多样的；不同的；相异的
+A diverse team can cooperate with each other and achieve a common goal.
+多元化的团队可以相互协作，实现共同目标。
+
+60、overweening 错误次数：3
+/ˌoʊvərˈwiːnɪŋ/ adj. 自负的，过于自信的，傲慢的
+In his overweening pride, he manipulated his staff like puppets.
+他过分自信骄傲，像控制玩偶一样操纵他的员工。
+
+61、anathema 错误次数：3
+/əˈnæθəmə/ n. 令人讨厌的人或事物
+Secondhand smoke is anathema to me.
+我非常厌恶二手烟。
+
+62、puerile 错误次数：3
+/ˈpjʊrəl/ adj. 幼稚的，孩子气的
+The little boy's puerile behavior got him in lots of trouble with his parents.
+这个小男孩幼稚的行为使他和父母间产生了很多问题。
+
+63、copious 错误次数：3
+/ˈkoʊpiəs/ adj. 丰富的，多产的
+I have copious documentation here to prove my point.
+
+我这里有大量的文献资料来证明我的观点。64、illustrious 错误次数：3
+/ɪˈlʌstriəs/ adj. 著名的，杰出的；辉煌的
+He is an illustrious action actor who is familiar to many Chinese.
+他是一位著名的动作演员，为许多中国人所熟知。
+
+65、cognizant 错误次数：3
+/ˈkɑːɡnɪzənt/ adj. 认识到的，意识到的，了解的
+I don't need to read this book: I'm already cognizant of everything in it.
+我不需要看这本书：我已经对里面所有的内容有所认知了。
+
+66、lamentable 错误次数：3
+/ˈlæməntəbl/ adj. 可悲的，可怜的，哀伤的
+Even though it's a lamentable situation, crying over it still won't help.
+虽然这是一个可悲的处境，为此哭泣还是没有用。
+
+67、uncouth 错误次数：3
+/ʌnˈkuːθ/ adj. 笨拙的；粗野的；不舒适的；陌生的
+His uncouth eating manner shocked the lady to some extent.
+男子粗野的吃相让那位女士受到了不少惊吓。
+
+68、debonair 错误次数：3
+/ˌdebəˈner/ adj. 温文尔雅的
+My life is that of a debonair playboy and man about town.
+我的生活就是那种温文尔雅的花花公子生活。
+
+69、tempestuous 错误次数：3
+/temˈpestʃuəs/ adj. 有暴风雨的，暴乱的，剧烈的
+Will we survive this tempestuous weather without being completely soaked?
+我们可以不被彻底淋湿地挺过这场暴风雨吗？
+
+70、miserly 错误次数：3
+/ˈmaɪzərli/ adj. 贪财的，吝啬的
+Despite being rich he is miserly with his money.
+尽管他很富有，他却很吝啬。
+
+71、contrived 错误次数：3
+/kənˈtraɪvd/ adj. 人为的；做作的；不自然的
+His reason for not handing in his homework was too contrived!
+
+他不交作业的理由也太牵强了！72、redolent 错误次数：3
+/ˈredələnt/ adj. 散发出…强烈气味的，（尤指气味）使人联想起…的
+The tea is redolent of mint and the ﬂower.
+茶水散发着薄荷味和花香。
+
+73、squalid 错误次数：3
+/ˈskwɑːlɪd/ adj. 污秽的，肮脏的，卑劣的
+The area around the garbage dumpster is really squalid.
+垃圾箱附近的这片区域十分肮脏。
+
+74、furtive 错误次数：3
+/ˈfɜːrtɪv/ adj. 偷偷的，秘密的，鬼鬼祟祟的，
+She might have escaped if she hadn't looked so furtive.
+如果不是看起来太鬼鬼祟祟，她可能已经逃脱了。
+
+75、predilection 错误次数：3
+/ˌpred(ə)lˈekʃ(ə)n/ n. 偏好
+His predilection for adventure and travel has taken him to many remote and exotic locations.
+他对冒险和旅行的偏好使他探索了许多偏远之地。
+
+76、retinue 错误次数：3
+/ˈretənuː/ n. 侍从，随员
+The leader struts around with his retinue of loyal followers in tow.
+这位领导昂首阔步，后面跟着一群忠臣的随员。
+
+77、imperious 错误次数：3
+/ɪmˈpɪriəs/ adj. 傲慢的，专横的
+"You must all listen to me," the man said in an imperious manner.
+“你们都得听我的。”这个男人态度专横地说。
+
+78、valediction 错误次数：3
+/ˌvælɪˈdɪkʃn/ n. (尤指正式演讲中的)告别，告别辞；告别词
+My friend gave me a ﬁnal valediction before I set off on my world tour.
+在我出发环球旅行前，朋友给了我最后的告别。
+
+79、redoubtable 错误次数：3
+/rɪˈdaʊtəbl/ adj. 可敬的，令人敬畏的
+He is a redoubtable boxer.
+他是一位令人敬畏的拳击手。
+
+80、rustic 错误次数：3
+/ˈrʌstɪk/ n. 乡下人，庄稼汉，粗汉 adj. 乡村的；质朴的；不精致的
+The small rustic farmhouse is in a beautiful natural setting.
+这个乡村小农场有着非常美丽的自然风光。
+
+81、exigency 错误次数：3
+/ˈeksɪdʒənsi/ n. 紧急（状态）；危急（关头）；危急关头
+If you have a Swiss Army Knife, you are prepared for any exigency.
+瑞士军刀在手，危急关头不愁。
+
+82、seethe 错误次数：3
+/siːð/ v. 沸腾；汹涌；激动
+The man seethes with anger.
+这个男人因为愤怒而激动。
+
+83、dislodge 错误次数：3
+/dɪsˈlɑːdʒ/ v. 驱逐；移去；取出
+The doctor dislodged the ﬁsh bone stuck in his throat.
+医生取出了哽在他喉咙里的鱼刺。
+
+84、pervasive 错误次数：3
+/pərˈveɪsɪv/ adj. 弥漫的，遍布的；深入人心的
+The smell of cigarettes was pervasive in the room; he smoked often.
+屋内烟味弥漫，他抽烟很勤。
+
+85、tepid 错误次数：3
+/ˈtepɪd/ adj. 微温的，不热烈的
+The frog feels ever so comfortable sitting in the tepid water.
+青蛙在温水中待着觉得很安逸。
+
+86、appellation 错误次数：3
+/ˌæpəˈleɪʃn/ n. 称呼，名称，名目
+The impartial and honorable Judge Bao earned the appellation "Bao Qingtian."
+铁面无私的包法官赢得了“包晴天”的称号。
+
+87、substantive 错误次数：3
+/ˈsʌbstəntɪv/ adj. 有实质的；大量的；真实的；独立存在的 n. 实词，名词，名词性词组
+When a rock is turned into gold, substantive change happens.
+
+当石头变成黄金，实质性的变化就发生了。88、ablution 错误次数：3
+/əb'luːʃən/ n. （宗教的）净礼，沐浴
+The man is performing ablution before entering the temple.
+这个男子在进入寺庙前进行宗教净礼。
+
+89、tacit 错误次数：3
+/ˈtæsɪt/ adj. 心照不宣的，静默的
+It is a tacit agreement that cell phones should be turned off during meeting.
+开会时手机应该保持静音是大家心照不宣的共识。
